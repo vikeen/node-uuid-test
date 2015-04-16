d2e12426528c291dd4f2c8d98f19a1c04fd57e68
John Rake <john.rake12@gmail.com>
2015-04-16 09:53:48 -0500

    Adding async and dynamic bucket selection

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 'use strict';
 
 var uuidGen = require('node-uuid');
+var async = require('async');
 
-var iterations = 10000000; // 10 million
-var results = {
-    'bucket1': [],
-    'bucket2': [],
-    'bucket3': [],
-    'bucket4': [],
-    'bucket5': []
-};
+var sampleSize = 10000000; // 10 million
+var buckets = 1000;
 
-/* Purely a security concern function */
+var results = {};
+
 function cleanse(uuid) {
     // cleanser to remove any non-alphanumeric and dash chars preventing xss
     if (!uuid.match(/^[a-zA-Z0-9-]+$/)) {
@@ -20,7 +16,6 @@ function cleanse(uuid) {
     return uuid;
 }
 
-/* strips the uuid to only numbers. Worth looking at. */
 function parse(/*str*/uuid) {
     var code = [],
         a = -1;
@@ -31,35 +26,47 @@ function parse(/*str*/uuid) {
 }
 
 function findModulo(/*number*/uuidParse) {
-    return uuidParse % 1000;
+    return uuidParse % buckets;
 }
 
-function placeInBucket(uuid, modulo) {
-    if (modulo <= 200) {
-        results['bucket1'].push(uuid);
-    } else if (modulo > 200 && modulo <= 400) {
-        results['bucket2'].push(uuid);
-    } else if (modulo > 400 && modulo <= 600) {
-        results['bucket3'].push(uuid);
-    } else if (modulo > 600 && modulo <= 800) {
-        results['bucket4'].push(uuid);
-    } else {
-        results['bucket5'].push(uuid);
+function placeInBucket(modulo) {
+    if (!results[modulo]) {
+        results[modulo] = 0;
+    }
+
+    results[modulo] += 1;
+}
+
+function progressReport(count, sampleSize, iterationSlice) {
+    if (count % (sampleSize / iterationSlice) === 0) {
+        console.log((count / sampleSize * 100).toFixed(2) + '%');
     }
 }
 
-for (var i = 0; i < iterations; i++) {
-    var uuid = uuidGen.v4(),
-        cleansed = cleanse(uuid),
-        parsed = parse(cleansed),
-        modulo = findModulo(parsed);
-
-    placeInBucket(uuid, modulo);
-
+function statisticsReport() {
+    console.log(results);
 }
 
-console.log('bucket1:', results['bucket1'].length);
-console.log('bucket2:', results['bucket2'].length);
-console.log('bucket3:', results['bucket3'].length);
-console.log('bucket4:', results['bucket4'].length);
-console.log('bucket5:', results['bucket5'].length);
+function main() {
+    var count = 0;
+
+    async.whilst(function() {
+        return count < sampleSize;
+    }, function(callback) {
+        setImmediate(function() {
+            count++;
+            placeInBucket(findModulo(parse(cleanse(uuidGen.v4()))));
+            progressReport(count, sampleSize, 10);
+            callback();
+        });
+    }, function(err) {
+        if (err) {
+            console.error(err);
+        } else {
+            statisticsReport();
+        }
+
+    });
+}
+
+main();
